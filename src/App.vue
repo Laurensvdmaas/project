d745dd911223aab56348c49864de1fe0168d611e
Laurens van der Maas <laurensvdmaas@gmail.com>
2020-09-23 10:39:36 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,27 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Test :msg="msg"></Test>
+    <div @click="msg = '1231234'">
+      Click here
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "./components/HelloWorld.vue";
+import { Test } from 'test-lib';
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld,
+    Test
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  msg = 'asdfadsfsadf';
+}
 </script>
 
 <style lang="scss">
